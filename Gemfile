2115dfe3ef3f1c0c847183d6cb443beabe67add5
narutaro <narusan@mac.com>
2016-05-03 18:33:33 -0700

    clean up

--- Gemfile
+++ Gemfile
@@ -1,4 +1,3 @@
 source 'https://rubygems.org'
-#gem 'mustache'
 gem 'rest-client', '~> 1.8'
 gem 'baby_erubis'
--- build.rb
+++ build.rb
@@ -1,15 +1,54 @@
 #!/usr/bin/env ruby
 
-require 'mustache'
+require 'baby_erubis'
+=begin
+require 'rest-client'
 
-data = {
-  "repo": [
-		"lepo": [ { "name": "resque" }, { "name": "hub" }, { "name": "rip" } ],
-		"lepo": [ { "name": "aresque" }, { "name": "ahub" }, { "name": "arip" } ]
-	]
+class Gist
+	attr_reader :last
+
+	def initialize
+		@url = "https://api.github.com/gists/"
+	end
+
+	def get_gist(gist_id)
+		r = RestClient.get(@url + gist_id)
+		json = JSON.parse(r)
+		#JSON.pretty_generate(json)
+	end
+end
+
+gid = open('target_gists.txt', 'r'){|f|
+	f.read.split("\n")
 }
 
-Mustache.template_file = './template_partial.mustache'
-puts Mustache.render(data)
+g = Gist.new
 
+gists = []
+gid.each do |id|
+	gist = g.get_gist(id)
+	gists << {
+		date: gist['updated_at'], #.match(/^(.+?)T.+?/)[1],
+		url: gist['html_url'],
+		desc: gist['description'],
+		title: gist['files'].values[0]['content'].match(/^#(\s*)(.*)\n/)[2]
+	}
+end
+
+gists.sort! do |a, b|
+	b[:date] <=> a[:date]
+end
+
+gists.each{|m|
+	p m
+}
+=end
+
+#td = [{:date=>"2016-05-03", :url=>"https://gist.github.com/9c9f8c244ebc654f44ec935c3ecde6ee", :desc=>"multipart gist", :title=>"part_1 portion"}, {:date=>"2016-05-03", :url=>"https://gist.github.com/a11661167a4caed22c7b74bc25f3a1a7", :desc=>"Sample of getting gists which is newly added since last time fetched. ", :title=>"!/usr/bin/env ruby"}, {:date=>"2016-04-24", :url=>"https://gist.github.com/3c75c37759830d34a154718c4fa4e7c0", :desc=>"embed testing", :title=>"Here you go."}]
+td = [{:date=>"2016-05-03", :url=>"https://gist.github.com/9c9f8c244ebc654f44ec935c3ecde6ee", :desc=>"multipart gist", :title=>"part_1 portion"}, {:date=>"2016-05-03", :url=>"https://gist.github.com/a11661167a4caed22c7b74bc25f3a1a7", :desc=>"Sample of getting gists which is newly added since last time fetched. ", :title=>"!/usr/bin/env ruby"}]
+
+template = BabyErubis::Html.new.from_file('index.html.erb', 'utf-8')
+context = {:gists=>td}
+output = template.render(context)
+print output
 
